8ca7833b9c978124f52b6b11fb7a74bb7dbee097
gidits13 <gidits13@gmail.com>
2022-08-25 18:08:30 +0300

    Добавьте файлы проекта.

---
+++ Chapter7p55/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Chapter7p55
+{
+    class Obj
+    {
+        public string Name;
+        public string Description;
+
+        public static string Parent;
+        public static int DaysInWeek;
+        public static int MaxValue;
+        static Obj()
+        {
+            Parent = "System.Object";
+            DaysInWeek = 7;
+            MaxValue = 2000;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
